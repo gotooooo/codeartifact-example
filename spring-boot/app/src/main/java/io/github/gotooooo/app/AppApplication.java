c4ce11d01455b893a3a22afb1655153f37bfa400
gotooooo <k.goto.123@gmail.com>
2022-05-23 19:44:24 +0900

    update app

--- spring-boot/app/src/main/java/io/github/gotooooo/app/AppApplication.java
+++ spring-boot/app/src/main/java/io/github/gotooooo/app/AppApplication.java
@@ -2,12 +2,14 @@ package io.github.gotooooo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.gotooooo.lib.Lib;
 
 @SpringBootApplication
 public class AppApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(AppApplication.class, args);
+    System.out.println(Lib.hello());
   }
 
 }

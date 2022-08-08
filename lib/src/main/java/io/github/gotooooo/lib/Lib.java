40867a4f8a525a19ad1272507538ede33ee74e5e
gotooooo <k.goto.123@gmail.com>
2022-08-08 18:26:41 +0900

    add javadoc

--- lib/src/main/java/io/github/gotooooo/lib/Lib.java
+++ lib/src/main/java/io/github/gotooooo/lib/Lib.java
@@ -1,6 +1,18 @@
 package io.github.gotooooo.lib;
 
+/**
+ * Multiple lines of Javadoc text are written here,
+ * wrapped normally...
+ */
 public class Lib {
+
+  /**
+   * This method return hello.
+   * 
+   * <p>This is an example.
+   * 
+   * @return hello
+   */
   public static String hello() {
     return "hello";
   }
--- lib/src/test/java/io/github/gotooooo/lib/LibTests.java
+++ lib/src/test/java/io/github/gotooooo/lib/LibTests.java
@@ -5,8 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests for {@link Lib} class.
+ */
 public class LibTests {
 
+  /**
+   * The {@code hello()} method return hello.
+   */
   @Test
   @DisplayName("hello test")
   void helloTestSuccessExapmle () {
